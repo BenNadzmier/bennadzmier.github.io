39195d4c9837b8f30ab51f1a87a8c8465edecaf4
BenNadzmier <blbensali@up.edu.ph>
2024-04-15 22:32:40 +0800

    feat: complete exer 7 specs minus refined css

---
+++ exer7/README.md
@@ -0,0 +1,5 @@
+Exercise 07: Front-end scripting
+Ben Nadzmier Bensali
+CMSC 100 UV3L
+
+Description: A simple food card generator :D
--- exer7/static_files/form.js
+++ exer7/static_files/form.js
@@ -3,55 +3,71 @@ function createFoodCard(name, description, imageURL, rank) {
 	// Create card element and set its innerHTML
 	// Add delete button with event listener
 	// Append card to food-cards section
-    const card = document.createElement("div");
-    card.style.width = "300px";
-    card.style.borderStyle = "solid";
-	card.innerHTML = `<p><b>Name</b>: ${name}</p><p><b>Description</b>: ${description}</p><b>URL:</b> ${imageURL}</p><p><b>Rank</b>: ${rank}</p><p><button class="delete-button">Delete</button></div><br><br>`;
+	const card = document.createElement("div");
+    card.className = "food-card";
+	card.id = rank;
+	card.innerHTML = 
+    `<img src="${imageURL}" alt="image of ${name}" style="width:250px">
+    <p><b>Name</b>: ${name}</p>
+    <p><b>Description</b>: ${description}</p>
+    <p><b>Rank</b>: ${rank}</p>
+    <p><button class="delete-button">Delete</button>
+    </div><br><br>`;
 
-
-    // Add event listener to delete button
+	// Add event listener to delete button
 	const deleteButton = card.querySelector(".delete-button");
 	deleteButton.addEventListener("click", function () {
 		deleteFoodCard(card);
 	});
 
-	// Append card to food-cards section
-	document.getElementById("food-cards").appendChild(card);
+	return card;
 }
 
 document
 	.getElementById("food-form")
 	.addEventListener("submit", function (event) {
 		event.preventDefault();
-
 		// Get input
 		fname = document.getElementById("fname").value;
 		desc = document.getElementById("desc").value;
 		url = document.getElementById("url").value;
 		rank = document.getElementById("rank").value;
 
-		// Validate form inputs
-		// TO-DO
+		//create food card and add to the DOM
+		const card = createFoodCard(fname, desc, url, rank);
 
-		// If inputs are valid, create food card and add to the DOM
-		createFoodCard(fname, desc, url, rank);
+		// Append card to food-cards section
+		const cardsList = document.getElementById("food-cards");
+
+		// If empty just add
+		if (cardsList.childNodes.length === 3) {
+			cardsList.appendChild(card);
+		}
+
+		// Else, do a sorted append
+		else {
+			for (node of cardsList.childNodes) {
+				if (Number(rank) < Number(node.id)) {
+					cardsList.insertBefore(card, node);
+				} else {
+					cardsList.appendChild(card);
+				}
+			}
+		}
 	});
 
+document
+	.getElementById("food-cards")
+	.addEventListener("delete", function (event) {
+		event.preventDefault();
 
-document.getElementById("food-cards").addEventListener("delete", function (event){
-    event.preventDefault();
+		card = document.getElementById("food-card");
 
-    card = document.getElementById("food-card")
+		deleteFoodCard(card);
+	});
 
-    deleteFoodCard(card);
-});
-    
 // Function to delete food card
 function deleteFoodCard(card) {
 	// Remove card from the DOM
-    document.getElementById("food-cards").removeChild(card);
+	document.getElementById("food-cards").removeChild(card);
 }
-
-//     // 6. Remove an element
-// const gamesList = document.getElementById("games-list")
-// //gamesList.removeChild(games[2])
